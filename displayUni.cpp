39f09dd2e48b6d7b4f2efa0d60a04ce60d87dac3
iooenilec <90336063+iooenilec@users.noreply.github.com>
2023-05-29 22:36:15 +0800

    Update displayUni.cpp
    added option for sort uni name

--- displayUni.cpp
+++ displayUni.cpp
@@ -1,4 +1,4 @@
-#include "searchUni.h"
+#include "search.h"
 void menu()
 {
     int choice;
@@ -6,8 +6,9 @@ void menu()
     {
         std::cout << "Main Menu" << std::endl;
         std::cout << "1. Display All Universities Information." << std::endl;
-        std::cout << "2. Binary Search" << std::endl;
-        std::cout << "3. Exit" << std::endl;
+        std::cout << "2. Quick Sort" << std::endl;  
+        std::cout << "3. Binary Search" << std::endl;
+        std::cout << "4. Exit" << std::endl;
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
@@ -17,9 +18,12 @@ void menu()
             displayUni();
             break;
         case 2:
-            displayTarget();
+            displaySortedUni();
             break;
         case 3:
+            displayTarget();
+            break;
+        case 4:
             exitProgram();
 
         case 22:
